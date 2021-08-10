bcdf03d2b421f4423a152c0cfeb61e1b6d3063a1
zetakelor <zetakelor@gmail.com>
2021-08-10 09:59:57 +0800

    增加二叉树构建类

---
+++ src/main/java/common/TreeNode.java
@@ -0,0 +1,20 @@
+package common;
+
+/**
+ * 构建二叉树
+ * @author fxzou
+ * @date 2021/8/10
+ * @since IntelliJ IDEA
+ */
+public class TreeNode {
+
+    public int val;
+
+    public TreeNode root;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
