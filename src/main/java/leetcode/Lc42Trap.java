ba3b95612e25945ee903f51d2ad32a9b6cd735ef
zetakelor <zetakelor@gmail.com>
2021-09-06 15:30:34 +0800

    commit lc42 接雨水

--- src/main/java/leetcode/Lc42Trap.java
+++ src/main/java/leetcode/Lc42Trap.java
@@ -16,7 +16,7 @@ public class Lc42Trap {
 
         int current = 0;
 
-        while (current <= height.length) {
+        while (current < height.length) {
 
             while (!stack.empty() && height[current] > height[stack.peek()]) {
                 int h = height[stack.peek()];
@@ -39,5 +39,40 @@ public class Lc42Trap {
             current++;
         }
         return count;
+    }
+
+    public int trap2(int[] height) {
+        int count = 0;
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < height.length; i++) {
+            // 如果栈不为空并且当前索引高度大于栈顶则一直循环
+            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
+                // 栈顶的小元素出栈
+                int curr = stack.peek();
+
+                stack.pop();
+
+                // 如果栈空了直接返回
+                if (stack.isEmpty()) {
+                    break;
+                }
+                // 栈顶的索引值就是前面的墙高度
+                int left = stack.peek();
+                // 当前的墙高度
+                int right = i;
+                //
+                int h = Math.min(height[left], height[right]) - height[curr];
+
+                count += (right - left - 1) * h;
+            }
+
+            stack.push(i);
+        }
+
+        return count;
+
+
     }
 }
